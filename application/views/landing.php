fe9a289e7be61bd1be672219a2d448ae70b2a024
cn.nadya@gmail.com <cn.nadya@gmail.com>
2020-08-30 21:36:35 +0700

    PUSH LANDING PAGE DULU YA

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,4 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-AYO BEGADANG SATU MINGGU LEBAAAAAAAAY
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>COBA BOOTSTRAP</title>
+
+</head>
+<body>
+
+
+</body>
+</html>
+
